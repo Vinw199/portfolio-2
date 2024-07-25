12ceb5676724369d618056ce61036df460469ef6
Vinw199 <vkvalenciasa@gmail.com>
2024-07-25 16:18:43 +0530

    projects section updated

--- data/index.ts
+++ data/index.ts
@@ -72,27 +72,27 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    title: "Peso",
+    des: "Create tables, dashboards, workflows and many more. Accounts receivable automation software.",
+    img: "/peso.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/three.svg", "/frm.svg"],
+    link: "https://app.pesohq.com",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    title: "Vorooki - Hosting Website",
+    des: "Deploy web app of all kinds, from large scale enterprise APIs to static websites for individuals. Fill out the form to try a demo of our platform",
+    img: "/vorooki.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://youthful-dijkstra-a8b171.netlify.app/",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
+    title: "Recordbook - Data management tool",
+    des: "A real Software-as-a-Service app of the popular mobile app 'RecordBook'",
+    img: "/rb.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    link: "https://web.rekord.in/",
   },
   {
     id: 4,
@@ -102,7 +102,7 @@ export const projects = [
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
     link: "https://github.com/adrianhajdin/iphone",
   },
-];
+]
 
 export const testimonials = [
   {
